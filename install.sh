63d80fca10c15b24d15835133d75497f60066c7c
Jeremiah Ayres <jeremiah.ayres@itpie.com>
2025-06-03 23:21:07 -0400

    Update to install.sh

--- install.sh
+++ install.sh
@@ -24,27 +24,11 @@ sudo systemctl start docker
 echo "---- Add permissions for sound ----"
 sudo usermod -aG audio $USER
 
-echo "---- Create project directory ----"
-mkdir -p ~/jukebox
-cd ~/jukebox
-
-echo "---- Clone or copy your repo ----"
-# If you host on Git just pull:
-# git clone https://github.com/your-repo/jukebox.git .
-
-# Otherwise manually copy your files here beforehand
-
 echo "---- Build Docker containers ----"
 docker-compose build
 
 echo "---- Starting containers ----"
 docker-compose up -d
 
-#echo "---- Adding Audio to the backend container ----"
-#docker run --rm \
-#  --device /dev/snd \
-#  --volume /etc/asound.conf:/etc/asound.conf \
-#  pitunes_backend
-#
 
 echo "---- All done ----"
