ea6b6c40140801e863238e404a4bbdcb2b2f614f
Jeremiah Ayres <ayresjeremiah@gmail.com>
2025-06-21 02:07:51 -0400

    fix Bug with docker-compose for frontend

--- frontend/src/app/services/config.service.ts
+++ frontend/src/app/services/config.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import {FeatureOptions} from 'src/app/models/featureOptions.model';
+import { Observable, of } from 'rxjs';
+import { isPlatformServer } from '@angular/common';
+import { FeatureOptions } from 'src/app/models/featureOptions.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  private isServer(): boolean {
+    return isPlatformServer(this.platformId);
+  }
+
   getConfig(): Observable<FeatureOptions> {
+    if (this.isServer()) {
+      // Prevent SSR crash by returning fallback
+      return of({ aiEnabled: false });
+    }
+
     return this.http.get<FeatureOptions>('/api/config');
   }
 }
